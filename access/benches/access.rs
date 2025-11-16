924455dc8c24e5364a0e229a546974ebdc435bbf
Will Murnane <will.murnane@gmail.com>
2025-11-16 07:20:59 -0500

    Benchmark against the JVM implementation

--- access/benches/access.rs
+++ access/benches/access.rs
@@ -16,19 +16,94 @@
 
 use std::hint::black_box;
 
-use access::expression;
+use access::{evaluate, expression};
 use criterion::{Criterion, criterion_group, criterion_main};
 
+use j4rs::{ClasspathEntry, InvocationArg, Jvm, JvmBuilder};
+
+fn instantiate_jvm() -> j4rs::errors::Result<Jvm> {
+    let jar = env!("JAR_LOCATION");
+    let entry = ClasspathEntry::new(jar);
+    let jvm: Jvm = JvmBuilder::new().classpath_entry(entry).build()?;
+
+    Ok(jvm)
+}
+
 pub fn criterion_benchmark(c: &mut Criterion) {
-    for sample in &[
-        "a",
-        "averylongtokenwhichjustkeepsgoingandneverstopsimeancomeon",
-        "a&b",
-        "a&b&c",
-        "a&(b&c)",
+    let jvm = instantiate_jvm().expect("Failed to instantiate JVM");
+    for (sample, tokens) in &[
+        ("a", "a"),
+        (
+            "averylongtokenwhichjustkeepsgoingandneverstopsimeancomeon",
+            "averylongtokenwhichjustkeepsgoingandneverstopsimeancomeon",
+        ),
+        ("a&b", "b,c,d"),
+        ("a&b&c", "a,b,c"),
+        ("a&(b&c)", "a,b,c"),
     ] {
-        c.bench_function(&format!("parse {}", sample), |b| {
-            b.iter(|| expression(black_box(sample)))
+        c.bench_function(&format!("java parse {sample}"), |b| {
+            b.iter(|| {
+                // Access.Builder access_builder = Access.builder();
+                let access_builder = jvm
+                    .invoke_static(
+                        "org.apache.accumulo.access.Access",
+                        "builder",
+                        InvocationArg::empty(),
+                    )
+                    .unwrap();
+                // Access access = access_builder.build()
+                let access = jvm
+                    .invoke(&access_builder, "build", InvocationArg::empty())
+                    .unwrap();
+
+                // Instantiate 'tokens' as array of strings
+                let tokens_jvm = jvm
+                    .create_java_array(
+                        "java.lang.String",
+                        &tokens
+                            .split(',')
+                            .map(|t| InvocationArg::try_from(t).unwrap())
+                            .collect::<Vec<_>>(),
+                    )
+                    .unwrap();
+                // Instantiate 'tokens' as a Set<String>
+                let tokens_set = jvm
+                    .invoke_static("java.util.Set", "of", &[InvocationArg::from(tokens_jvm)])
+                    .unwrap();
+                // Create an Authorizations object
+                let auths = jvm
+                    .invoke(
+                        &access,
+                        "newAuthorizations",
+                        &[InvocationArg::from(tokens_set)],
+                    )
+                    .unwrap();
+                let evaluator = jvm
+                    .invoke(&access, "newEvaluator", &[InvocationArg::from(auths)])
+                    .unwrap();
+                // Instantiate 'sample' as java string
+                let sample_string = jvm
+                    .create_instance(
+                        "java.lang.String",
+                        &[InvocationArg::try_from(*sample).unwrap()],
+                    )
+                    .expect("Convert to java instance");
+                let _ = jvm
+                    .invoke(
+                        &evaluator,
+                        "canAccess",
+                        &[InvocationArg::from(sample_string)],
+                    )
+                    .unwrap();
+            });
+        });
+
+        c.bench_function(&format!("rust parse {sample}"), |b| {
+            b.iter(|| {
+                let ex = expression(black_box(sample)).unwrap();
+                let tok = access::tokens(black_box(tokens)).unwrap();
+                let _ = evaluate(&ex, &tok);
+            })
         });
     }
 }
